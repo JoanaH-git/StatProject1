447a81e6d944e1147f52fe7d41ccf26615eb3e6d
braxus32 <79334546+braxus32@users.noreply.github.com>
2021-10-28 11:45:13 -0700

    Updated Eric Aguirre part 2 for group scatterplot
    Uses library(patchwork)
    must have "options(scipen = 999)" at top of file

--- aguirre_data-viz.R
+++ aguirre_data-viz.R
@@ -1,3 +1,4 @@
+options(scipen =999)
 # Load Packages -----------------------------------------------------------
 
 library(dplyr) # For data wrangling
@@ -5,6 +6,7 @@ library(tidyr) # For data tidying
 library(ggthemes) # For presentation plots
 library(ggplot2) # For plotting data
 library(stat242) # For data
+library(patchwork) # For aesthetics
 
 # Load data from part I ---------------------------------------------------
 
@@ -31,8 +33,71 @@ ggplot(project_data, aes(life_exp, fill = as.factor(year))) +
 # Scatter plots -----------------------------------------------------------
 
 ## Life Expectancy v net users
-ggplot(project_data, aes(net_users, life_exp, color=continent))+
-  geom_point()
+asia <- project_data %>%
+  filter(continent=="Asia")
+africa <- project_data %>%
+  filter(continent=="Africa")
+europe <- project_data %>%
+  filter(continent=="Europe")
+s_amer  <- project_data %>%
+  filter(continent=="South America")
+n_amer <- project_data %>%
+  filter(continent=="North America")
+oceania <- project_data %>%
+  filter(continent=="Oceania")
+
+asia_plot <- ggplot(asia, aes(net_users, life_exp))+
+  geom_point()+
+  labs(
+    x = "Internet Users (millions)",
+    y = "Life Expectancy"
+  )+
+  theme(axis.text.x = element_text(angle = 45))
+
+africa_plot <- ggplot(africa, aes(net_users, life_exp))+
+  geom_point()+
+  labs(
+    x = "Internet Users (millions)",
+    y = "Life Expectancy"
+  )+
+  theme(axis.text.x = element_text(angle = 45))
+
+euro_plot <- ggplot(europe, aes(net_users, life_exp))+
+  geom_point()+
+  labs(
+    x = "Internet Users (millions)",
+    y = "Life Expectancy"
+  )+
+  theme(axis.text.x = element_text(angle = 45))
+
+s_am_plot <- ggplot(s_amer, aes(net_users, life_exp))+
+  geom_point()+
+  labs(
+    x = "Internet Users (millions)",
+    y = "Life Expectancy"
+  )+
+  theme(axis.text.x = element_text(angle = 45))
+
+n_am_plot <- ggplot(n_amer, aes(net_users, life_exp))+
+  geom_point()+
+  labs(
+    x = "Internet Users (millions)",
+    y = "Life Expectancy"
+  )+
+  theme(axis.text.x = element_text(angle = 45))
+
+ocea_plot <- ggplot(oceania, aes(net_users, life_exp))+
+  geom_point()+
+  labs(
+    x = "Internet Users (millions)",
+    y = "Life Expectancy"
+  )+
+  theme(axis.text.x = element_text(angle = 45))
+
+asia_plot + africa_plot + euro_plot + s_am_plot + n_am_plot + ocea_plot+
+  plot_annotation(title = "Life Expectancy v. Internet Users by Continent",
+                  subtitle = "1979 - 2018",
+                  caption = "Source: Gapminder")
 
 ## Life Expectancy v Year
 ggplot(project_data, aes(year, life_exp, color=continent))+
